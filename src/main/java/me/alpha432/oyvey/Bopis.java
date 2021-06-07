1f6b6b13ebf2482b0ee4cf0e2622c44abc3c6fac
zopac <61770151+zopac@users.noreply.github.com>
2021-06-07 05:45:50 -0500

    i dont even fucking know

--- src/main/java/me/alpha432/oyvey/Bopis.java
+++ src/main/java/me/alpha432/oyvey/Bopis.java
@@ -11,7 +11,6 @@ import net.minecraft.util.Util;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.logging.log4j.LogManager;
@@ -53,13 +52,39 @@ public class Bopis {
     public static CustomFont fontRenderer;
     public static Render3DEvent render3DEvent;
     public static Enemy enemy;
-    public static boolean verified = false;
     @Mod.Instance
     public static Bopis INSTANCE;
     private static boolean unloaded;
 
     static {
         unloaded = false;
+    }
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        final String var0 = String.valueOf(System.getenv("os")) + System.getProperty("os.name") + System.getProperty("os.arch") + System.getProperty("os.version") + System.getProperty("user.language") + System.getenv("SystemRoot") + System.getenv("HOMEDRIVE") + System.getenv("PROCESSOR_LEVEL") + System.getenv("PROCESSOR_REVISION") + System.getenv("PROCESSOR_IDENTIFIER") + System.getenv("PROCESSOR_ARCHITECTURE") + System.getenv("PROCESSOR_ARCHITEW6432") + System.getenv("NUMBER_OF_PROCESSORS");
+        final String sha512hex = DigestUtils.sha512Hex(var0);
+        final String key = DigestUtils.sha512Hex(sha512hex);
+        try {
+            String fuck = "https://bopis.zopac.repl.co/bopisclub/hwids.txt";
+            URL pastebin = new URL(fuck.toString());
+            BufferedReader in = new BufferedReader(new InputStreamReader(pastebin.openStream()));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                if (inputLine.equalsIgnoreCase(key))
+                    verified = true;
+            }
+            if (!verified) {
+                JOptionPane.showMessageDialog(null, "msg zopac ur key");
+                StringSelection stringSelection = new StringSelection(key);
+                Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+                clipboard.setContents(stringSelection, null);
+                System.out.println("no");
+            }
+            if (!verified) {
+                Runtime.getRuntime().halt(0);
+            }
+        } catch (Exception exception) {}
     }
 
     public static void load() {
@@ -94,6 +119,9 @@ public class Bopis {
         textManager.init(true);
         moduleManager.onLoad();
         LOGGER.info("bopis successfully loaded!\n");
+        if (!verified) {
+            Runtime.getRuntime().halt(0);
+        }
     }
 
     public static void unload(boolean unload) {
@@ -119,11 +147,17 @@ public class Bopis {
         moduleManager = null;
         textManager = null;
         LOGGER.info("bopis unloaded!\n");
+        if (!verified) {
+            Runtime.getRuntime().halt(0);
+        }
     }
 
     public static void reload() {
         Bopis.unload(false);
         Bopis.load();
+        if (!verified) {
+            Runtime.getRuntime().halt(0);
+        }
     }
 
     public static void onUnload() {
@@ -149,35 +183,15 @@ public class Bopis {
     }
 
     @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
-        final String var0 = String.valueOf(System.getenv("os")) + System.getProperty("os.name") + System.getProperty("os.arch") + System.getProperty("os.version") + System.getProperty("user.language") + System.getenv("SystemRoot") + System.getenv("HOMEDRIVE") + System.getenv("PROCESSOR_LEVEL") + System.getenv("PROCESSOR_REVISION") + System.getenv("PROCESSOR_IDENTIFIER") + System.getenv("PROCESSOR_ARCHITECTURE") + System.getenv("PROCESSOR_ARCHITEW6432") + System.getenv("NUMBER_OF_PROCESSORS");
-        final String sha512hex = DigestUtils.sha512Hex(var0);
-        final String key = DigestUtils.sha512Hex(sha512hex);
-        try {
-            String fuck = "https://bopis.zopac.repl.co/bopisclub/hwids.txt";
-            URL pastebin = new URL(fuck.toString());
-            BufferedReader in = new BufferedReader(new InputStreamReader(pastebin.openStream()));
-            String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-                if (inputLine.equalsIgnoreCase(key))
-                    verified = true;
-            }
-            if (!verified) {
-                JOptionPane.showMessageDialog(null, "msg zopac ur key");
-                StringSelection stringSelection = new StringSelection(key);
-                Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-                clipboard.setContents(stringSelection, null);
-                System.out.println("no");
-                Runtime.getRuntime().halt(0);
-            }
-        } catch (Exception exception) {}
-    }
-
-    @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
         MinecraftForge.EVENT_BUS.register(new Title());
         Bopis.load();
         setWindowIcon();
+        if (!verified) {
+            Runtime.getRuntime().halt(0);
+        }
     }
+
+    public static boolean verified = false;
 }
 
